9c421fc3686a550c5b2fd0117a93da7184d43e02
Eric Jinks <ericjinks@gmail.com>
2019-08-06 09:50:50 +1000

    Generate random lat/lon

--- app.R
+++ app.R
@@ -1,57 +1,35 @@
 # app.R
 library(geojsonio)
-library(jsonlite)
 
+#' CORS allows requests made from external urls to be accepted
 #' @filter cors
 cors <- function(res) {
   res$setHeader("Access-Control-Allow-Origin", "*")
   plumber::forward()
 }
 
-#' @get /healthCheck
-function(){
-  list(200, "Health Check passed", Sys.getenv("hostname"))
-}
+#' Serve GEOJSON containing 3 points surrounding central coordinate
+#' @param lat latitude of central coordinate
+#' @param lon lon of central coordinate
+#' @post /geojson
+function(lat, lon){
+  lat <- as.numeric(lat)
+  lon <- as.numeric(lon)
+  maxRange <- 0.1
 
-#' Echo back the input
-#' @param msg The message to echo
-#' @get /echo
-function(msg=""){
-  list(msg = paste0("The message is: '", msg, "'"))
-}
-
-#' Plot a histogram
-#' @png
-#' @get /plot
-function(){
-  rand <- rnorm(100)
-  hist(rand)
-}
-
-#' Return the sum of two numbers
-#' @param a The first number to add
-#' @param b The second number to add
-#' @post /sum
-function(a, b){
-  as.numeric(a) + as.numeric(b)
-}
-
-#' Serve GEOJSON
-#' @json
-#' @get /geojson
-function(){
   # Create data
+  n <- 70
   df <- data.frame(
-    id = c('cam1', 'cam2', 'cam3'),
-    lat = c(-28.1708, -28.1704, -28.17),
-    lon = c(153.5473, 153.5478, 153.5482)
+    lat = runif(n, min = lat - maxRange, max = lat + maxRange),
+    lon = runif(n, min = lon - maxRange, max = lon + maxRange),
+    magnitude = runif(n, min = 0, max = 1)
   ) 
+
   # create geojson string from dataframe
-  dataString <- geojson_json(df)
-  # create R dataframe from JSON string
-  data <- fromJSON(dataString)
-  # return data
-  data
+  geojsonString <- geojson_json(df)
+
+  # data to return
+  geojsonString
 }
 
 print('app.R running')
